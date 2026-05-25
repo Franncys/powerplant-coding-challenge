106eb5169ffc3d17bc80418bf24c54305f8622e0
Franncys <matosfranciscosantos@gmail.com>
2026-05-25 13:53:03 +0200

    feat: added swagger for quick tests

---
+++ src/PowerPlantCodingChallenge.Api/Controller/ProductionPlanController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerPlantCodingChallenge.Api.Controller
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductionPlanController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult CalculateProductionPlan()
+		{
+			return Ok("");
+		}
+
+	}
+}
